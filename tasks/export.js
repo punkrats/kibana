1d833803ad3e6f97c09ce0d6678873dcfa554839
punkrats <andre@webwarelab.com>
2014-12-02 20:00:30 +0100

    Add export task and script

---
+++ tasks/export.js
@@ -0,0 +1,28 @@
+module.exports = function (grunt) {
+  grunt.registerTask('export', [
+    'get_build_props',
+    'clean:target',
+    'clean:build',
+    'require_css_deps:copy',
+    'less',
+    'copy:kibana_src',
+    'clean:dev_only_plugins',
+    'touch_config',
+    'replace:build_props',
+    'requirejs',
+    'clean:unneeded_source_in_build'
+    // 'copy:server_src',
+    // 'download_jruby',
+    // 'install_gems',
+    // 'warble',
+    // 'replace:dist',
+    // 'copy:dist',
+    // 'compile_dist_readme',
+    // 'chmod_kibana',
+    // 'make_plugin_dir',
+    // 'copy:plugin_readme',
+    // 'describe_bundled_plugins',
+    // 'copy:versioned_dist',
+    // 'create_packages'
+  ]);
+};
