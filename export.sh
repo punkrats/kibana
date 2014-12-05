6894d88f5be6fb1d822d56317aa100ac3a26bd30
punkrats <andre@webwarelab.com>
2014-12-05 16:02:59 +0100

    Remove static config on export

--- export.sh
+++ export.sh
@@ -10,4 +10,6 @@ sed -i '' 's:\./:/kibana/:' $DEST/require.config.js
 
 sed -i '' 's:\.\./\.\./bower_components/:../bower_components/:g' $DEST/styles/main.css
 
-echo '{"port":5601,"host":"0.0.0.0","kibanaIndex":".kibana","defaultAppId":"dashboard","request_timeout":60,"shard_timeout":30000,"verifySSL":true,"html5Mode":true, "plugins":["plugins/kibana/index","plugins/dashboard/index","plugins/discover/index","plugins/metric_vis/index","plugins/settings/index","plugins/table_vis/index","plugins/vis_types/index","plugins/visualize/index"]}' > $DEST/config
+rm $DEST/config
+
+# echo '{"port":5601,"host":"0.0.0.0","kibanaIndex":".kibana","defaultAppId":"dashboard","request_timeout":60,"shard_timeout":30000,"verifySSL":true,"html5Mode":true, "plugins":["plugins/kibana/index","plugins/dashboard/index","plugins/discover/index","plugins/metric_vis/index","plugins/settings/index","plugins/table_vis/index","plugins/vis_types/index","plugins/visualize/index"]}' > $DEST/config
